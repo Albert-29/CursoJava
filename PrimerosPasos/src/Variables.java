a84088722f5d68493d4f4a7b0311e459f811f072
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-06-06 12:15:17 -0600

    Agregando la clase de variables

---
+++ PrimerosPasos/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        //Se declara la variable con el tipo de dato 
+        //y posteriormente el identificador o nombre de la variable
+        byte edad;
+        //Se inicializa la variable con un valor de 35 utilizando el signo de "igual" "="
+        edad=35;
+
+        System.out.println("Mensaje en consola: " + edad);
+    }
+
+}
