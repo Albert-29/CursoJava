c79faded540d749b540597e6f45aa224be6117db
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-06-06 12:22:31 -0600

    Agregando comentarios a la PrimeraClase

--- PrimerosPasos/src/PrimeraClase.java
+++ PrimerosPasos/src/PrimeraClase.java
@@ -1,3 +1,10 @@
+/*Una clase se crea de la siguiente manera.
+//Utilizando el modificador de acceso 
+utilizando en este caso la palabra reservada del lenguaje "public" 
+seguido de otro palabra reservada "class" y 
+por ultimo el nombre de la clase empezando siempre con la primer letra en mayuscula
+Si la clase son dos o mas palabras utilizaras la notacion de camello (camel case) 
+Las palabras van junatas pero empezando siempre con una letra mayuscula "PrimeraClase" */
 public class PrimeraClase {
     public static void main(String args[]) {
         System.out.println("Hola mundo, soy Dev Java.");
