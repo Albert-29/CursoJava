78bf5446f9880b782e49cbfd10a07e88d7d6be6b
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-06-08 17:19:05 -0600

    Trabajando con los paquetes y utilizando la palabra import

--- PrimerosPasos/src/Prueba.java
+++ PrimerosPasos/src/Prueba.java
@@ -6,19 +6,17 @@ import java.util.Scanner;
  * En esta clase veremos como trabajan los paquetes de java y 
  * cual es la razon por la que se utiliza la instruccion "import"
  * Cual es el paquete por defecto de java "java.lang"
- * cuales son las razones por las cuales java organiza de esta forma sus paquetes y clases. 
+ * cuales son las razones por las cuales java organiza de esta forma sus paquetes y clases 
  */
 
 public class Prueba {
     public static void main(String[] args) {
-        
+
         String nombre;
 
         Scanner miObjeto;
 
         Locale miObjeto2;
-
-
 
     }
 
