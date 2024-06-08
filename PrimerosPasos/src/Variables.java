df0dbb514bea8d88bd485bf31bc51daa1efda231
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-06-08 14:12:03 -0600

    Agregando comentarios en la clase de variables

--- PrimerosPasos/src/Variables.java
+++ PrimerosPasos/src/Variables.java
@@ -3,11 +3,17 @@ public class Variables {
     public static void main(String[] args) {
         //Se declara la variable con el tipo de dato 
         //y posteriormente el identificador o nombre de la variable
-        byte edad;
+        //byte edad;
+        int edad;
         //Se inicializa la variable con un valor de 35 utilizando el signo de "igual" "="
         edad=35;
 
+        //Metodo de la consola o terminal que nos permite mostrar un mensaje desde la consola.
         System.out.println("Mensaje en consola: " + edad);
+
+        edad = 75;
+
+        System.out.println("Nueva edad: " + edad);
     }
 
 }
