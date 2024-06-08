212faf21f9aa2e642c8b685c4da0b1e6dd5c57d4
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-06-08 14:34:31 -0600

    Agrgando la leccion de operadores

---
+++ PrimerosPasos/src/Operadores.java
@@ -0,0 +1,29 @@
+public class Operadores {
+    /*
+     * Operadores
+     *  Aritmeticos +, -, *, /
+     *  Logicos <, >, !=, ==, &&
+     *  Incremento y decremento ++, --, +=variable, -=variable
+     *  Concatenacion +
+     */
+
+    public static void main(String[] args) {
+       
+        //int a = 5;
+        //int b;
+
+        double a=5;
+        double b;
+
+        b=7;
+
+        double c = b / a;
+
+        //c++;
+        //c-=6;
+
+        System.out.println(c);
+        
+    }
+
+}
