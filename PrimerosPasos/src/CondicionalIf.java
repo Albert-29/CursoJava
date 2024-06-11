b182decbcffd03ba7fa3dc9f55dcb935f1e82aa3
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-06-11 12:36:44 -0600

    Agegando la clase de condicionales If

---
+++ PrimerosPasos/src/CondicionalIf.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class CondicionalIf {
+    public static void main(String[] args) {
+    
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce tu edad");
+        int edad = entrada.nextInt();
+
+       /*  if (edad >= 18) {
+
+            System.out.println("Eres mayor de edad");
+        }
+        else {
+            System.out.println("Eres menor de edad");
+        } */
+       
+        if (edad<18) {
+            System.out.println("Eres adolescente");
+        } else if(edad<40) {
+            System.out.println("Eres joven");
+        } else if(edad <65){
+            System.out.println("Eres maduro");
+        }
+        else {
+            System.out.println("Cuidate");
+        }
+
+
+
+    }
+
+}
