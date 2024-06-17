577f2ce35231f33bc67cf18470b08c479cc07cc1
Alberto Miranda Pacheco <albert59.gs@gmail.com>
2024-06-17 15:34:40 -0600

    Agregando la clase de bucle o ciclos for

---
+++ PrimerosPasos/src/BucleFor.java
@@ -0,0 +1,10 @@
+public class BucleFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Alberto " + i);
+        }
+        for (int j = 10; j > 0; j--) {
+            System.out.println("Alberto " + j);
+        }
+    }
+}
--- PrimerosPasos/src/PesoIdeal.java
+++ PrimerosPasos/src/PesoIdeal.java
@@ -8,6 +8,18 @@ public class PesoIdeal {
         do {
             genero = JOptionPane.showInputDialog("Introduce tu genero(H/M)");
         } while (genero.equalsIgnoreCase("H")==false && genero.equalsIgnoreCase("M")==false);
+
+        int altura = Integer.parseInt(JOptionPane.showInputDialog("Introduce altura en cm: "));
+        int peso = 0;
+
+        if (genero.equalsIgnoreCase("H")) {
+            peso=altura-110;
+        }
+        else if (genero.equalsIgnoreCase("M")) {
+            peso=altura-120;
+            
+        }
+        System.out.println("Tu peso ideal es  " + peso + " Kilos");
     }
 
 }
